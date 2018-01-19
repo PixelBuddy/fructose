4c089067629e5bb0133bf24ad20a9eeb5111ece6
EC2 Default User <ec2-user@ip-172-31-12-168.us-east-2.compute.internal>
2018-01-19 08:24:47 +0000

    Created Pages controller. Added explore, home, index and profile pages. Changed root URL to index page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+    
+    #Define Root URL
+    root 'pages#index'
+    
+    #Define route for Pages
+  get 'pages/home' => 'pages#home'
+  get 'pages/profile' => 'pages#profile'
+  get 'pages/explore' => 'pages/explore'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
